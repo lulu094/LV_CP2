323fc42edc097cd09728e4de52b0a702f467096a
lulu094 <lucci.venanciomostaceli@ucas-eu.net>
2026-01-08 09:52:04 -0700

    no le llame a mami

---
+++ individual_projects/financial_calc.py
@@ -0,0 +1,37 @@
+# LV 1st Financial Calculator
+# Compound Interest Calculator
+"""make an inner function for the calculator 
+make a dictionary of the interest, initial deposit, constribution amount,
+the user gets to choose if they want the interest weekly, montly or yearly"""
+
+# Budget Allocator
+""" Make it a function
+a formula for how long it will take for the user to reach their goal
+formula for the percentages so every month or so it gets in their account"""
+
+# Sale Price Calculator (aply discounts price)
+""" function discharge the monthly losts like foood, rent, clothes, and that"""
+
+
+# Tip Calculator
+"""Give the user discount options robably make it a function
+make sure it knows how to do the math an that"""
+
+
+
+# Main Loop 
+#Ask the user for how much they want to deposit and the goal they want to reach
+#And make it have an en b asking or just letting the user choose by themselve when they want to finish
+
+# Compound Interest Calculator (inner function)
+# inside o this function has to be the tip calculator
+# make sure the calculator adds the tip 
+# need to expand more in detail
+
+# Buget Allocator  function
+# make sure to use percentages to divide an income into spenin categories like savins, entertainme, and food
+# make sure this all loops well so we can discharge the monthly debt
+
+# Sale Price Calc (function make sure it goes hand to hand with the other functions)
+# apply discounts to prices
+# make sure it is i decimals/ percentages
