71523338e8327d00d8595595301d8e1ab1707b2a
lulu094 <lucci.venanciomostaceli@ucas-eu.net>
2026-01-22 09:51:46 -0700

    oui oui je suis french

---
+++ individual_projects/password_generator.py
@@ -0,0 +1,38 @@
+# LV 1st Random Password Generator
+
+import random
+# Main Menu function
+
+#   Display menu options:
+#     1. Generate a Password
+#     2. Exit
+#   Ask user to choose an option - make sure it is a number
+#  if choice == 1
+#         call generate_password
+#   elif choice == 2
+#         end program
+#         display Goodbye!
+#    else
+#         print  Please enter a number
+
+# def generate_password():
+#     """Generates a random password based on user criteria"""
+# make a list for all the lowercase letters, uppercase, numbers, and special characters
+# when user tels us the length of the password, make sure it is a number 
+
+
+# function for the different assword requirements
+# 
+
+
+
+
+
+
+# Main loop
+# make sure user enter either y/n for the questions
+# ask user for password length
+# ask user if they want uppercase letters
+# ask user if they want numbers
+# ask user if they want special characters
+# give them 1-4 options based on their choices
