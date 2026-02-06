dbd7188c17e1d8c1ecee96cb245aab572cb43ae1
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-02-06 09:52:36 -0700

    algo es algo dijo un calvo

---
+++ individual_projects/movie.py
@@ -0,0 +1,32 @@
+# LV 1st Movie
+
+# access movie list CSV
+
+# Main Menu function
+# Options 
+# 1 Search / Get Recommendations 
+# 2 Print Full Movie List 
+# 3 Exit
+
+
+# Search recomendation function
+# Give the user options to choose from so we can narrow the options down to two main ones that would fit the criteria
+# Genre 
+# Ask the user for what genre they would watch
+# then give the list of every movie that would fit the genre just so we can go of off that
+
+# Director
+# Whose directors movies they like the most
+# Based on the genre they chose keep shortening the options of movies to the directors they also liked
+# If there isnt a match then display THERE WASN'T A MATCH WITH UR CRITERIA
+
+# Actor
+# If there is a specific actor they like keep shortening the list
+# Else display THERE WASN'T A MATCH WITH UR CRITERIA
+
+# Aun no se como hacerlo ya que falte pero mi logica es que voy hacerlo pasar por una variable que es un bucle
+# Primero hago un bucle por el que va a pasar despues del main menu
+# Este bucle va a ser muy importante ya que es en el que vamos a importar el csv
+# Tambien vamos a hacer la lista mas y mas chica hasta que llegue a 2 peliculas
+# Esto es muy importante asi que tengo que mejorar si quiero una a
+# mis companeras de al lado an hecho bastante
