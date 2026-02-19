86c54fb4d09c26037ee9908d8cf291cef2fa0d6a
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-02-19 09:52:20 -0700

    my code

---
+++ individual_projects/word counter/word_counter.py
@@ -0,0 +1,10 @@
+# LV 1st Word Counter
+
+# main menu 
+# 1. update document info
+# 2. View document
+# 3. Add content to document 
+# 4. Exit
+# Ask user to enter your choice (1-4)
+
+# Update document info pull from txt
