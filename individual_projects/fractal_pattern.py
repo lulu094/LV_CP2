17e02d0ffc6d88d3e9b1bb228ae9b8beef55827b
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-02-25 09:52:20 -0700

    pseudocode

---
+++ individual_projects/fractal_pattern.py
@@ -0,0 +1,14 @@
+# LV 1st Fractal Pattern Generator
+
+# import turtle
+
+# REQUIREMENTS
+# Let the user customize the recursion depth and color of the fractal
+# Make Turtle draw he Sierpinski Triangle wiht what the user has chosen
+# Recursion depth should be between 1 and 5
+# Use Base Case in recursion to stop it from being infinite
+
+# Main Menu
+# Welcome user
+# ask for how they want their depth to be range 1-5
+# Ask for the color they want but check it with the colors turtle accepts
